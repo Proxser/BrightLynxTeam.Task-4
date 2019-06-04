56e1ab512b327ca72590bdff69eafdab9f2ec73e
Slava Petrovsky <kuromukura@mail.ru>
2019-06-04 14:03:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,288 +1,102 @@
-(() => {
-  const Timer = {
+// Массив ячеек шахматной доски
+const chessboard = document.querySelectorAll('.cell');
 
-    // Функция создания объекта "Таймер""
-    create(el) {
-      this.$el = document.querySelector(el);
-      this.mins = 0;
-      this.secs = 0;
-      this.msecs = 0;
-      this.intervalMsecs = null;
-      this.intervalMins = null;
-      this.intervalSecs = null;
-      this.isRun = false;
-    },
+// Функция конвертации (преобразования) индекса вида "n" к виду "i, j"
+function convertToXY(value) {
+  return [parseInt(value / 8, 10), parseInt(value % 8, 10)];
+}
 
-    // Функция добавления нулей заполнителей перед цифрами
-    addZeros(num, numDigit) {
-      const nLength = numDigit || 2;
-      let n = String(num);
-      while (n.length < nLength) {
-        n = `0${n}`;
-      }
-      return n;
-    },
+// Функция конвертации (преобразования) индекса вида "i, j" к виду "n"
+function convertXYToIndex(arr) {
+  return parseInt((arr[0] * 8) + arr[1], 10);
+}
 
-    // Функция возвращающая текущее значение таймера
-    currentValue() {
-      return `${Timer.addZeros(Timer.mins)}:${Timer.addZeros(Timer.secs)}.${Timer.addZeros(Timer.msecs, 3)}`;
-    },
+// Функция сброса цветов ячеек шахматной доски до изначального
+function resetChessboard() {
+  // Проходим массив (по i, j), конвертируем i и j в целочисленный индекс
+  // ячейчки в массиве "chessboard", создаём массив в котором будем хранить,
+  // добавляемые к ячейке CSS-классы, если по строка нечётная, то цвета ячеек
+  // начинают чередоваться с белого цвета, иначе с чёрного
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      const x = convertXYToIndex([i, j]);
+      const classes = ['cell'];
+      const ifY = i % 2 !== 0;
+      const ifX = j % 2 !== 0;
 
-    // Функция отображения изменения значения таймера
-    displayChange() {
-      Timer.$el.innerHTML = `<b>${Timer.currentValue()}</b>`;
-    },
+      chessboard[x].classList = '';
 
-    // Функция изменения значения таймера
-    change() {
-      if (Timer.msecs < 1000) {
-        Timer.msecs += 1;
+      if (ifY) {
+        if (!ifX) {
+          classes.push('black');
+        } else {
+          classes.push('white');
+        }
+      } else if (ifX) {
+        classes.push('black');
       } else {
-        Timer.msecs = (Timer.msecs % 1000) + 1;
-        if (Timer.secs < 60) {
-          Timer.secs += 1;
-        } else {
-          Timer.secs = (Timer.secs % 60) + 1;
-          Timer.mins += 1;
-        }
+        classes.push('white');
       }
-      Timer.displayChange();
-    },
 
-    // Функция сброса значения таймера
-    reset() {
-      Timer.msecs = 0;
-      Timer.secs = 0;
-      Timer.mins = 0;
-      Timer.displayChange();
-    },
+      chessboard[x].classList = classes.join(' ');
+    }
+  }
+}
 
-    // Функция зайпуска таймера
-    start() {
-      console.log('Timer.start');
-      clearInterval(Timer.interval);
-      Timer.interval = setInterval(Timer.change, 1);
-      Timer.isRun = !Timer.isRun;
-    },
+// Функция нахождения всех возможных ходов конём
+function horseSteps(arr) {
+  const x = arr[1];
+  const y = arr[0];
+  const result = [];
 
-    // Функция остановки таймера
-    stop() {
-      console.log('Timer.stop');
-      clearInterval(Timer.interval);
-      Timer.isRun = !Timer.isRun;
-      Timer.displayChange();
-    },
+  const topLeft = (y - 2) >= 0 && (x - 1) >= 0;
+  const topRight = (y - 2) >= 0 && (x + 1) <= 7;
 
-  }; // Timer End
+  const leftTop = (x - 2) >= 0 && (y - 1) >= 0;
+  const leftBottom = (x - 2) >= 0 && (y + 1) <= 7;
 
-  const Field = {
+  const rightTop = (x + 2) <= 7 && (y - 1) >= 0;
+  const rightBottom = (x + 2) <= 7 && (y + 1) <= 7;
 
-    // Функция создания элемента массива элементов (ячеек) игрового поля
-    createElement(color) {
-      const $el = document.createElement('div');
-      $el.classList.add('cell', 'hidden');
-      const obj = {
-        el: $el,
-        color,
-      };
-      return obj;
-    },
+  const bottomLeft = (y + 2) <= 7 && (x - 1) >= 0;
+  const bottomRight = (y + 2) <= 7 && (x + 1) <= 7;
 
-    // Функция генерации массива элементов игрового поля
-    generate(colors) {
-      const result = [];
-      for (let i = 0; i < colors.length; i++) {
-        const color = colors[i];
-        result.push(this.createElement(color));
+  if (topLeft) result.push(convertXYToIndex([y - 2, x - 1]));
+  if (topRight) result.push(convertXYToIndex([y - 2, x + 1]));
+
+  if (rightTop) result.push(convertXYToIndex([y - 1, x + 2]));
+  if (rightBottom) result.push(convertXYToIndex([y + 1, x + 2]));
+
+  if (bottomRight) result.push(convertXYToIndex([y + 2, x + 1]));
+  if (bottomLeft) result.push(convertXYToIndex([y + 2, x - 1]));
+
+  if (leftTop) result.push(convertXYToIndex([y - 1, x - 2]));
+  if (leftBottom) result.push(convertXYToIndex([y + 1, x - 2]));
+
+  return result;
+}
+
+// Добавления обработчика события на клик по ячейке шахматной доски
+for (let i = 0; i < chessboard.length; i++) {
+  chessboard[i].addEventListener('click', (event) => {
+    // Очищаем поле, находим индекс кликнутого элемента в массиве ячеек доски,
+    // чтобы успешно найти все возможные ходы коня из данной ячейки, окрашиваем
+    // выбранную ячейку в синий, а возможные ходы в зелёный
+    let index;
+    resetChessboard();
+
+    for (let j = 0; j < chessboard.length; j++) {
+      if (chessboard[j] === event.target) {
+        index = j;
+        break;
       }
-      return result;
-    },
+    }
 
-    // Функция генерации случайного целого числа
-    getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    },
+    event.target.classList.add('blue');
+    const horsesteps = horseSteps(convertToXY(index));
 
-    // Функция перемешивания элементов массива
-    mix(inputArray) {
-      const resultArray = inputArray;
-      let j;
-      let temp;
-      for (let i = 0; i < resultArray.length; i++) {
-        j = this.getRandomInt(0, 16);
-        temp = resultArray[i];
-        resultArray[i] = resultArray[j];
-        resultArray[j] = temp;
-      }
-      return resultArray;
-    },
-
-    // Функция перемешивания массива элементов (ячеек) игрового поля
-    shuffle(inputArray) {
-      this.colors = this.mix(inputArray);
-    },
-
-    // Функция отображения игрового поля
-    display() {
-      for (let i = 0; i < this.cells.length; i++) {
-        this.$el.append(this.cells[i].el);
-      }
-    },
-
-    // Функция создания объекта игрового поля
-    create(el) {
-      this.$el = document.querySelector(el);
-      this.colors = ['gray', 'black', 'red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-      this.shuffle(this.colors.concat(this.colors));
-      this.cells = this.generate(this.colors);
-      this.countPairsFound = 0;
-      this.isSecondClick = false;
-      this.prevSelectElement = null;
-      this.selectElement = null;
-      this.selectColor = null;
-      this.index = null;
-      this.moves = 0;
-      this.display();
-    },
-
-    // Функция добавления обработчика события клика по ячейке
-    binding(handleClick) {
-      for (let i = 0; i < this.cells.length; i++) {
-        this.cells[i].el.addEventListener('click', handleClick);
-      }
-    },
-
-    // Функция удаления обработчика события клика по ячейке
-    unbinding(handleClick) {
-      for (let i = 0; i < this.cells.length; i++) {
-        this.cells[i].el.removeEventListener('click', handleClick);
-      }
-    },
-
-    // Функция сброса фона у всех ячеек игрового поля
-    bgResetAll() {
-      for (let i = 0; i < this.cells.length; i++) {
-        this.cells[i].el.classList = '';
-        this.cells[i].el.classList.add('cell', 'hidden');
-      }
-    },
-
-    // Функция сброса параметров игры до изначальных (до нажатия на кнопку "Старт")
-    reset() {
-      this.countPairsFound = 0;
-      this.isSecondClick = false;
-      this.prevSelectElement = null;
-      this.selectElement = null;
-      this.selectColor = null;
-      this.index = null;
-      this.moves = 0;
-      this.bgResetAll();
-      this.unbinding(this.handleClick);
-      this.display();
-    },
-
-    // Функция запуска игры
-    start() {
-      this.reset();
-      this.binding(this.handleClick);
-      this.display();
-    },
-
-    // Функция поиска индекса выбранного (кликнутого) элемента (ячейки) в массиве
-    // элементов игрового поля
-    findInCells() {
-      let result;
-      for (let i = 0; i < this.cells.length; i++) {
-        if (this.cells[i].el === this.selectElement) {
-          result = i;
-          break;
-        }
-      }
-      return result;
-    },
-
-    // Функция, содержащая логику для обработки второго клика
-    ifSecondClick() {
-      // Если это клик по другой ячейке (2-й клик) и если их цвета совпадают, то
-      // увеличиваем кол-во найденных пар на единицу, если кол-во найденных пар
-      // равно восьми, то игра окончена на экран выводится результат и поздравление.
-      // Если цвета различаются, то обе ячейки скрываются.
-      // Если это первый клик, то запоминаем выбранный элемент, его цвет и ставим
-      // флаг "true" в "isSecondClick", чтобы при 2-ом клике, начать сравнивать
-      // ячейки игрового поля на цвета.
-      if (Field.isSecondClick) {
-        Field.isSecondClick = false;
-        if (Field.cells[Field.index].color === Field.selectColor) {
-          Field.countPairsFound += 1;
-          if (Field.countPairsFound === 8) {
-            Timer.stop();
-            const record = Timer.currentValue();
-            const winText = `Вы выйграли! Кол-во ходов: ${Field.moves}\nВаше время: ${record}`;
-            Timer.displayChange();
-            alert(winText);
-            Field.reset();
-            Timer.reset();
-            Button.$el.innerText = 'Старт';
-          }
-        } else {
-          Field.prevSelectElement.classList.add('hidden');
-          Field.selectElement.classList.add('hidden');
-        }
-      } else {
-        Field.selectColor = Field.cells[Field.index].color;
-        Field.prevSelectElement = Field.selectElement;
-        Field.isSecondClick = true;
-      }
-    },
-
-    // Функция-обработчик события клика по ячейке
-    handleClick(event) {
-      // Запоминаем выбранный элемент и находим его индекс в массиве элементов
-      // игрового поля, чтобы узнать и присвоить соответствующий цвет
-      Field.selectElement = event.target;
-      Field.index = Field.findInCells();
-
-      // Если элемент скрыт, то открываем его (присваиваем фону выбранного эл-та
-      // соответствующий цвет), запоминаем ход, ждём следующего клика, если цвета
-      // не равны, то через 250 мс скрываем оба элемента
-      if (Field.selectElement.classList.contains('hidden')) {
-        Field.moves += 1;
-        Field.selectElement.classList.remove('hidden');
-        Field.selectElement.classList.add(Field.cells[Field.index].color);
-        setTimeout(Field.ifSecondClick, 250);
-      }
-    },
-
-  }; // Field End
-
-  let Button = {
-
-    // Функция создания объекта кнопки
-    create(el) {
-      this.$el = document.querySelector(el);
-      this.setup();
-    },
-
-    // Функция установки функции-обработчика на событие клика по кнопке "Старт"
-    setup() {
-      this.$el.addEventListener('click', () => {
-        console.log('Button.click');
-        Field.start();
-        if (!Timer.isRun) {
-          Timer.start();
-          Button.$el.innerText = 'Стоп';
-        } else {
-          Field.reset();
-          Timer.stop();
-          Timer.reset();
-          Button.$el.innerText = 'Старт';
-        }
-      });
-    },
-
-  }; // Button End
-
-  Field.create('.field');
-  Timer.create('.timer');
-  Button.create('.start');
-})();
+    for (let j = 0; j < horsesteps.length; j++) {
+      chessboard[horsesteps[j]].classList.add('green');
+    }
+  });
+}
